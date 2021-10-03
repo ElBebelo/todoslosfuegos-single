b76e4c00b2c58da970552f6826543bcb251bfa6d
unknown <nacho71197@hotmail.com>
2021-10-03 19:58:45 -0300

    Version semi-lista

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 function mostrarImagen(image){
-    var img = document.getElementsByClassName(image);
+    var img = document.getElementById(image);
     var modal = document.getElementById('myModal');
-    var modalImg = document.getElementById("img01");
+    var modalImg = document.getElementById("modal-img");
     img.onclick = function(){
       modal.style.display = "block";
-      modalImg.style.backgroundImage.valueOf = img.style.backgroundImage.valueOf; 
+      modalImg.style.maxWidht="650px";
+      modalImg.style.marginTop="0%";
+      modalImg.style.transform="scale(1)";
+      modalImg.style.animationName="zoom";
+      modalImg.src = img.src; 
+    }
+    var span = document.getElementsByClassName("close")[0];
+    
+    span.onclick = function() { 
+      modal.style.display = "none";
+    }
+    
+  }
+  function mostrarImagenHeight(image){
+    var img = document.getElementById(image);
+    var modal = document.getElementById('myModal');
+    var modalImg = document.getElementById("modal-img");
+    img.onclick = function(){
+      modal.style.display = "block";
+      modalImg.style.maxWidht="250px";
+      modalImg.style.marginTop="-18%";
+      modalImg.style.animationName="zoom2"
+      modalImg.style.transform="scale(0.5)"
+      modalImg.src = img.src;
     }
     var span = document.getElementsByClassName("close")[0];
     
